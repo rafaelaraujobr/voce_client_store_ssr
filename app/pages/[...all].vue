b8e8530388ededeb02017b96b6947b7fcfff58aa
Rafael <rflaraujodev@gmail.com>
2025-09-09 17:16:21 -0300

    Refatora o middleware de inquilinos para armazenar informações detalhadas do tenant, incluindo slug, host, domínio base e subdomínios reservados. Atualiza a página de erro 404 para exibir o tenant de forma mais clara, melhorando a experiência do usuário ao lidar com inquilinos inexistentes.

--- app/pages/[...all].vue
+++ app/pages/[...all].vue
@@ -3,14 +3,15 @@ import { useTenant } from "~/composables/tenent.composable";
 definePageMeta({
   layout: "empty",
 });
-const tenant = useTenant()
+const tenant = useTenant();
 </script>
 <template>
   <div
     class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
   >
     <div>
-      <div style="font-size: 30vh">404 {{ tenant || "Tenant não encontrado" }}</div>
+      <div style="font-size: 30vh">404</div>
+      <div>{{ tenant }}</div>
 
       <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
 
