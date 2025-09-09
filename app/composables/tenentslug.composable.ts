28fc805731bdd73e98a8b5279a00dbc0f07e1b07
Rafael <rflaraujodev@gmail.com>
2025-09-09 13:57:19 -0300

    Refatora o composable de slug do inquilino para melhorar a legibilidade, reorganizando a ordem das variáveis e garantindo a consistência do código. Além disso, adiciona logs no middleware para facilitar a depuração e monitoramento do evento e do host. Essas mudanças visam aprimorar a clareza e a manutenção do código.

--- app/composables/tenentslug.composable.ts
+++ app/composables/tenentslug.composable.ts
@@ -1,7 +1,7 @@
 export const useTenantSlug = () => {
-  const ev = import.meta.server ? useRequestEvent() : null
-  const fromHost = ev?.context?.tenantSlug
-  const route = useRoute()
-  const fromRoute = route.params.slug as string | undefined
-  return fromHost || fromRoute || null
-}
+  const route = useRoute();
+  const ev = import.meta.server ? useRequestEvent() : null;
+  const fromHost = ev?.context?.tenantSlug;
+  const fromRoute = route.params.slug as string | undefined;
+  return fromHost || fromRoute || null;
+};
