ba9aa3184abc957165725eb19dbead5760410f6b
Rafael <rflaraujodev@gmail.com>
2025-09-09 17:07:49 -0300

    Atualiza a página de erro 404 para exibir uma mensagem padrão caso o tenant não seja encontrado, melhorando a experiência do usuário ao lidar com inquilinos inexistentes.

--- app/pages/[...all].vue
+++ app/pages/[...all].vue
@@ -10,7 +10,7 @@ const tenant = useTenant()
     class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
   >
     <div>
-      <div style="font-size: 30vh">404 {{ tenant }}</div>
+      <div style="font-size: 30vh">404 {{ tenant || "Tenant não encontrado" }}</div>
 
       <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
 
