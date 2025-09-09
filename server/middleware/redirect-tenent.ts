65cef5f247393ed1ae81a816c9923a8ada6a9e41
Rafael <rflaraujodev@gmail.com>
2025-09-09 14:20:10 -0300

    Adiciona logs de depuração no middleware de redirecionamento de inquilinos, registrando o slug e a URL da requisição. Essas alterações visam facilitar a monitorização e a identificação de problemas durante o redirecionamento.

--- server/middleware/redirect-tenent.ts
+++ server/middleware/redirect-tenent.ts
@@ -1,6 +1,9 @@
 export default defineEventHandler((event) => {
+  console.log("redirect-tenent");
   const slug = (event as any)?.context?.tenantSlug
   const url = getRequestURL(event)
+  console.log("slug", slug);
+  console.log("url", url);
   if (slug && url.pathname === '/') {
     return sendRedirect(event, `/in/${slug}`, 307)
   }
--- server/middleware/tenent.ts
+++ server/middleware/tenent.ts
@@ -2,7 +2,6 @@ export default defineEventHandler((event) => {
   console.log("tenent");
   const { host } = getRequestURL(event)
   console.log("host", host);
-  console.log("event", event);
   const base = process.env.BASE_DOMAIN || 'dominio.com.br'
   const isSub = host.endsWith(base) && host !== base && !host.startsWith('www.')
   if (isSub) event.context.tenantSlug = host.replace(`.${base}`, '')
