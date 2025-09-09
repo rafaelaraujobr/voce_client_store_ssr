a156b6547e70c374f082cba2c09582942d4d1c13
Rafael <rflaraujodev@gmail.com>
2025-09-09 10:49:46 -0300

    Refatora o middleware de redirecionamento de inquilinos para melhorar a legibilidade e a estrutura do código. A lógica de verificação do slug e o redirecionamento foram organizados em um bloco condicional, aprimorando a clareza do fluxo de execução.

--- server/middleware/redirect-tenent.ts
+++ server/middleware/redirect-tenent.ts
@@ -1,7 +1,7 @@
-import { useRequestEvent } from "nuxt/app"
-
 export default defineEventHandler((event) => {
-    const slug = (useRequestEvent() as any)?.context?.tenantSlug
-    const url = getRequestURL(event)
-    if (slug && url.pathname === '/') return sendRedirect(event, `/in/${slug}`, 307)
-  })
+  const slug = (event as any)?.context?.tenantSlug
+  const url = getRequestURL(event)
+  if (slug && url.pathname === '/') {
+    return sendRedirect(event, `/in/${slug}`, 307)
+  }
+})
