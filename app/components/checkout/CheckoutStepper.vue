2346422643a35fc371b109b1f978e62fef81d337
raphaelcaires <raphaelcaires@gmail.com>
2025-11-03 16:05:09 -0300

    refactor: enhance responsiveness and layout for product showcase components

--- app/components/checkout/CheckoutStepper.vue
+++ app/components/checkout/CheckoutStepper.vue
@@ -1,109 +1,58 @@
 <template>
-  <q-stepper
-    ref="stepper"
-    v-model="step"
-    color="primary"
-    animated
-    flat
-    bordered
-  >
+  <q-stepper ref="stepper" v-model="step" color="primary" animated flat bordered>
     <q-step :name="1" title="Seus dados" icon="settings" :done="step > 1">
       <div class="text-weight-medium text-subtitle1 q-mb-md">
         Dados pessoais
       </div>
-      <div class="row q-col-gutter-md">
-        <div class="col-12 col-md-6">
+      <div class="row q-col-gutter-sm q-col-gutter-md-md">
+        <!-- Nome completo -->
+        <div class="col-12 col-sm-12 col-md-6">
           <q-input v-model="user.name" label="Nome completo" outlined dense />
         </div>
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="user.email"
-            label="E-mail"
-            type="email"
-            outlined
-            dense
-          />
+        <!-- E-mail -->
+        <div class="col-12 col-sm-12 col-md-6">
+          <q-input v-model="user.email" label="E-mail" type="email" outlined dense />
         </div>
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="user.phone"
-            label="Telefone"
-            type="tel"
-            outlined
-            dense
-          />
+        <!-- Telefone -->
+        <div class="col-12 col-sm-6 col-md-6">
+          <q-input v-model="user.phone" label="Telefone" type="tel" outlined dense />
         </div>
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="user.cpf"
-            outlined
-            dense
-            label="CPF"
-            type="text"
-            mask="###.###.###-##"
-          />
+        <!-- CPF -->
+        <div class="col-12 col-sm-6 col-md-6">
+          <q-input v-model="user.cpf" outlined dense label="CPF" type="text" mask="###.###.###-##" />
         </div>
       </div>
       <div class="text-weight-medium text-subtitle1 q-my-md">
         Endereço de entrega
       </div>
-      <div class="row q-col-gutter-md">
-        <div class="col-12 col-md-6">
+      <div class="row q-col-gutter-sm q-col-gutter-md-md">
+        <!-- CEP -->
+        <div class="col-12 col-sm-6 col-md-4">
           <q-input v-model="address.zipcode" label="CEP" outlined dense />
         </div>
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="address.street"
-            label="Rua"
-            type="text"
-            outlined
-            dense
-          />
+        <!-- Rua -->
+        <div class="col-12 col-sm-6 col-md-8">
+          <q-input v-model="address.street" label="Rua" type="text" outlined dense />
         </div>
-        <div class="col-12 col-md-4">
-          <q-input
-            v-model="address.number"
-            label="Número"
-            type="number"
-            outlined
-            dense
-          />
+        <!-- Número -->
+        <div class="col-6 col-sm-4 col-md-3">
+          <q-input v-model="address.number" label="Número" type="number" outlined dense />
         </div>
-        <div class="col-12 col-md-4">
-          <q-input
-            v-model="address.complement"
-            outlined
-            dense
-            label="Complemento"
-            type="text"
-          />
+        <!-- Complemento -->
+        <div class="col-6 col-sm-8 col-md-5">
+          <q-input v-model="address.complement" outlined dense label="Complemento" type="text" />
         </div>
-        <div class="col-12 col-md-4">
-          <q-input
-            v-model="address.neighborhood"
-            label="Bairro"
-            type="text"
-            outlined
-            dense
-          />
+        <!-- Bairro -->
+        <div class="col-12 col-sm-12 col-md-4">
+          <q-input v-model="address.neighborhood" label="Bairro" type="text" outlined dense />
         </div>
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="address.city"
-            label="Cidade"
-            type="text"
-            outlined
-            dense
-          />
+        <!-- Cidade -->
+        <div class="col-12 col-sm-8 col-md-6">
+          <q-input v-model="address.city" label="Cidade" type="text" outlined dense />
         </div>
-        <div class="col-12 col-md-6">
-          <q-input
-            v-model="address.state"
-            label="Estado"
-            type="text"
-            outlined
-            dense
-          />
+        <!-- Estado -->
+        <div class="col-12 col-sm-4 col-md-6">
+          <q-input v-model="address.state" label="Estado" type="text" outlined dense />
         </div>
       </div>
     </q-step>
@@ -121,19 +70,21 @@
 
     <template #navigation>
       <q-stepper-navigation>
-        <q-btn
-          color="primary"
-          :label="step === 2 ? 'Finalizar compra' : 'Avançar'"
-          @click="stepper?.next()"
-        />
-        <q-btn
-          v-if="step > 1"
-          flat
-          color="primary"
-          label="Voltar"
-          class="q-ml-sm"
-          @click="stepper?.previous()"
-        />
+        <div class="row q-gutter-sm">
+          <!-- Botão Avançar/Finalizar -->
+          <div class="col-12 col-sm-auto">
+            <q-btn
+              color="primary"
+              :label="step === 2 ? 'Finalizar compra' : 'Avançar'"
+              class="full-width"
+              @click="stepper?.next()"
+            />
+          </div>
+          <!-- Botão Voltar -->
+          <div v-if="step > 1" class="col-12 col-sm-auto">
+            <q-btn flat color="primary" label="Voltar" class="full-width" @click="stepper?.previous()" />
+          </div>
+        </div>
       </q-stepper-navigation>
     </template>
   </q-stepper>
--- app/components/showcase/ShowCaseList.vue
+++ app/components/showcase/ShowCaseList.vue
@@ -2,41 +2,18 @@
   <q-infinite-scroll ref="scrollTargetRef" :offset="800" debounce="500">
     <!-- @load="onScroll" -->
     <div class="row q-col-gutter-md full-width">
-      <div
-        v-for="product in products"
-        :key="product.id"
-        class="col-xs-12 col-sm-6 col-md-3"
-      >
-        <q-card
-          flat
-          bordered
-          class="cursor-pointer product-card border-default"
-          role="button"
-          :aria-label="`Ver produto ${product.name}`"
-          tabindex="0"
-          @click="navigateToProduct(product)"
-          @keydown.enter="navigateToProduct(product)"
-          @keydown.space="navigateToProduct(product)"
-        >
-          <q-card-section
-            class="row q-pa-none q-ma-none justify-center items-center"
-          >
+      <div v-for="product in products" :key="product.id" class="col-xs-12 col-sm-6 col-md-4">
+        <q-card flat bordered class="cursor-pointer product-card border-default" role="button"
+          :aria-label="`Ver produto ${product.name}`" tabindex="0" @click="navigateToProduct(product)"
+          @keydown.enter="navigateToProduct(product)" @keydown.space="navigateToProduct(product)">
+          <q-card-section class="row q-pa-none q-ma-none justify-center items-center">
             <div class="col-4 col-md-12 bg-default q-py-sm">
-              <q-img
-                :src="product.image"
-                fit="contain"
-                spinner-color="primary"
-                class="product-image"
-                height="118px"
-                spinner-size="82px"
-                loading="lazy"
-                placeholder-src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI2VlZSIvPgogIDx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBmb250LWZhbWlseT0iQXJpYWwiIGZvbnQtc2l6ZT0iMTQiIGZpbGw9IiNhYWEiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGR5PSIuM2VtIj5JbWFnZW08L3RleHQ+Cjwvc3ZnPg=="
-              >
+              <q-img :src="product.image" fit="contain" spinner-color="primary" class="product-image" height="118px"
+                spinner-size="82px" loading="lazy"
+                placeholder-src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI2VlZSIvPgogIDx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBmb250LWZhbWlseT0iQXJpYWwiIGZvbnQtc2l6ZT0iMTQiIGZpbGw9IiNhYWEiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGR5PSIuM2VtIj5JbWFnZW08L3RleHQ+Cjwvc3ZnPg==">
                 <template #error>
-                  <div
-                    class="absolute-full flex flex-center bg-secondary text-white text-center"
-                    style="max-width: 150px; margin: 0 auto"
-                  >
+                  <div class="absolute-full flex flex-center bg-secondary text-white text-center"
+                    style="max-width: 150px; margin: 0 auto">
                     Imagem indisponível
                   </div>
                 </template>
@@ -48,128 +25,83 @@
                   <q-item-label lines="2" class="q-mb-sm text-grey-6">{{
                     product?.name
                   }}</q-item-label>
-                  <div
-                    v-if="
-                      product.price &&
-                      product.price_discount &&
-                      product.price_discount < product.price
-                    "
-                  >
-                    <q-item-label
-                      lines="2"
-                      class="text-subtitle1 text-negative"
-                      style="text-decoration: line-through"
-                    >
-                      {{ numberToReal(product?.price) }}</q-item-label
-                    >
+                  <div v-if="
+                    product.price &&
+                    product.price_discount &&
+                    product.price_discount < product.price
+                  ">
+                    <q-item-label lines="2" class="text-subtitle1 text-negative" style="text-decoration: line-through">
+                      {{ numberToReal(product?.price) }}</q-item-label>
                     <div class="row">
                       <q-item-label lines="2" class="text-h5 text-weight-bold">
                         {{
                           numberToReal(product?.price_discount)
-                        }}</q-item-label
-                      >
+                        }}</q-item-label>
                     </div>
                   </div>
                   <div v-else>
                     <q-item-label lines="2" class="text-h5 text-weight-bold">
-                      {{ numberToReal(product?.price) }}</q-item-label
-                    >
+                      {{ numberToReal(product?.price) }}</q-item-label>
                   </div>
-                  <div
-                    v-if="
-                      product?.installments &&
-                      product.installments.installment > 2
-                    "
-                    class="text-grey-6 text-caption"
-                  >
+                  <div v-if="
+                    product?.installments &&
+                    product.installments.installment > 2
+                  " class="text-grey-6 text-caption">
                     <b>3x</b>
                     sem juros de
                     <b>{{
                       numberToReal(
                         (product.installments.value /
                           product.installments.installment) *
-                          3
+                        3
                       )
                     }}</b>
                   </div>
                 </q-item-section>
               </q-item>
             </div>
-            <q-badge
-              v-if="
-                product.price &&
-                product.price_discount &&
-                product.price_discount < product.price
-              "
-              :style="{
-                backgroundColor: '#FFC107',
-                top: '10px',
-                borderRadius: '0 5px 5px 0',
-              }"
-              text-color="black"
-              class="text-subtitle1 text-weight-bold absolute-top-left"
-              >-
+            <q-badge v-if="
+              product.price &&
+              product.price_discount &&
+              product.price_discount < product.price
+            " :style="{
+              backgroundColor: '#FFC107',
+              top: '10px',
+              borderRadius: '0 5px 5px 0',
+            }" text-color="black" class="text-subtitle1 text-weight-bold absolute-top-left">-
               {{
                 getDiscountPercent(product.price, product.price_discount)
-              }}%</q-badge
-            >
-            <q-btn
-              v-if="!productsInCart.find((p) => p.id === product.id)"
-              color="primary"
-              icon="mdi-cart-plus"
-              class="absolute-top-right"
-              flat
-              dense
-              :style="{
-                top: '8px',
+              }}%</q-badge>
+            <q-btn v-if="!productsInCart.find((p) => p.id === product.id)" color="primary" icon="mdi-cart-plus"
+              :class="$q.screen.lt.md ? 'absolute-bottom-right' : 'absolute-top-right'" flat dense :style="{
+                bottom: $q.screen.lt.md ? '8px' : 'auto',
+                top: $q.screen.lt.md ? 'auto' : '8px',
                 right: '8px',
-              }"
-              @click.stop="addProductToCart(product)"
-            />
-            <q-btn
-              v-else
-              color="negative"
-              icon="mdi-cart-remove"
-              class="absolute-top-right"
-              flat
-              dense
-              :style="{
-                top: '8px',
+              }" @click.stop="addProductToCart(product)" />
+            <q-btn v-else color="negative" icon="mdi-cart-remove"
+              :class="$q.screen.lt.md ? 'absolute-bottom-right' : 'absolute-top-right'" flat dense :style="{
+                bottom: $q.screen.lt.md ? '8px' : 'auto',
+                top: $q.screen.lt.md ? 'auto' : '8px',
                 right: '8px',
-              }"
-              @click.stop="removeProductFromCart(product)"
-            />
+              }" @click.stop="removeProductFromCart(product)" />
           </q-card-section>
         </q-card>
       </div>
     </div>
     <div class="row justify-center items-center q-mt-md q-py-md">
-      <q-btn
-        v-if="!loadingProducts && products.length < totalProducts"
-        :loading="loadingProducts"
-        color="primary"
-        label="Carregar mais produtos"
-        class="q-mt-md"
-        padding="sm md"
-        @click="loadMoreProducts(slug as string)"
-      />
+      <q-btn v-if="!loadingProducts && products.length < totalProducts" :loading="loadingProducts" color="primary"
+        label="Carregar mais produtos" class="q-mt-md" padding="sm md" @click="loadMoreProducts(slug as string)" />
     </div>
-    <div
-      v-if="isLoadingMore"
-      class="row justify-center items-center q-mt-md q-py-md"
-    >
+    <div v-if="isLoadingMore" class="row justify-center items-center q-mt-md q-py-md">
       <q-spinner-dots color="primary" size="40px" />
       <span class="q-ml-sm text-grey-6">Carregando mais produtos...</span>
     </div>
 
-    <div
-      v-else-if="
-        !loadingProducts &&
-        products.length >= totalProducts &&
-        products.length > 0
-      "
-      class="row justify-center items-center q-mt-md q-py-md"
-    >
+    <div v-else-if="
+      !loadingProducts &&
+      products.length >= totalProducts &&
+      products.length > 0
+    " class="row justify-center items-center q-mt-md q-py-md">
       <span class="text-grey-6">Todos os produtos foram carregados</span>
     </div>
   </q-infinite-scroll>
