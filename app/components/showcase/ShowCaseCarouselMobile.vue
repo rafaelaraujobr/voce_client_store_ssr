658eb6d1c9db8c41e05ba07cfe1ea96610b015aa
Rafael Araujo <60323056+rafaelaraujobr@users.noreply.github.com>
2025-12-10 18:51:08 -0300

    Refactor ShowCaseCarouselMobile component to enhance layout and improve responsiveness
    - Adjusted image height and structure for better mobile display.
    - Consolidated price display logic, ensuring clarity for discounted prices.
    - Streamlined component layout for improved user experience on mobile devices.

--- app/components/showcase/ShowCaseCarouselMobile.vue
+++ app/components/showcase/ShowCaseCarouselMobile.vue
@@ -17,61 +17,67 @@
         :key="product.id"
         :name="index + 1"
         class="bg-default row no-wrap items-center justify-center product-card"
-      >
-        <q-item class="col-12">
-          <q-item-section>
-            <q-img
-              :src="product.image"
-              fit="contain"
-              height="300px"
-              class="product-image"
-              spinner-size="82px"
-            />
-          </q-item-section>
-          <q-item-section class="q-gutter-y-sm">
-            <q-item-label
-              class="text-subtitle1 q-mb-sm text-weight-medium"
-              lines="2"
-            >
-              {{ product.name }}
-            </q-item-label>
-               <div
-                    v-if="
-                      product.price &&
-                      product.price_discount &&
-                      product.price_discount < product.price
-                    "
+        >
+        <div class="col-12">
+          <q-img
+            :src="product.image"
+            fit="contain"
+            height="200px"
+            class="product-image"
+            spinner-size="82px"
+          />
+        </div>
+        <div class="col-12 q-mb-xl text-center">
+          <q-item>
+            <q-item-section class="q-gutter-y-sm">
+              <q-item-label
+                class="text-subtitle1 q-mb-sm text-weight-medium"
+                lines="2"
+              >
+                {{ product.name }}
+              </q-item-label>
+              <div
+                v-if="
+                  product.price &&
+                  product.price_discount &&
+                  product.price_discount < product.price
+                "
+              >
+                <q-item-label
+                  lines="2"
+                  class="text-subtitle1 text-negative text-center"
+                  style="text-decoration: line-through"
+                >
+                  {{
+                    $t("from") + " " + numberToReal(product?.price)
+                  }}</q-item-label
+                >
+                <div class="row">
+                  <q-item-label lines="2" class="text-h5 text-weight-bold">
+                    {{
+                      $t("by") + " " + numberToReal(product?.price_discount)
+                    }}</q-item-label
                   >
-                    <q-item-label
-                      lines="2"
-                      class="text-subtitle1 text-negative"
-                      style="text-decoration: line-through"
-                    >
-                      {{ $t("from") + " " + numberToReal(product?.price) }}</q-item-label
-                    >
-                    <div class="row">
-                      <q-item-label lines="2" class="text-h5 text-weight-bold">
-                        {{
-                           $t("by") + " " + numberToReal(product?.price_discount)
-                        }}</q-item-label
-                      >
-                    </div>
-                  </div>
-                  <div v-else>
-                    <q-item-label lines="2" class="text-h5 text-weight-bold">
-                      {{ numberToReal(product?.price) }}</q-item-label
-                    >
-                  </div>
-            <q-item-label
-              v-if="product.installments?.installment"
-              class="text-grey-7 text-caption row items-center no-wrap"
-            >
-              {{ product.installments?.installment }}x {{ $t("from") }}
-              <div class="q-mx-sm">
-                {{ numberToReal(product.installments?.value) }}
+                </div>
               </div>
-              {{ $t("by") }}
-            </q-item-label>
+              <div v-else>
+                <q-item-label lines="2" class="text-h5 text-weight-bold">
+                  {{ numberToReal(product?.price) }}</q-item-label
+                >
+              </div>
+              <q-item-label
+                v-if="product.installments?.installment"
+                class="text-grey-7 text-caption row items-center no-wrap"
+              >
+                {{ product.installments?.installment }}x {{ $t("from") }}
+                <div class="q-mx-sm">
+                  {{ numberToReal(product.installments?.value) }}
+                </div>
+                {{ $t("by") }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+          <div class="row justify-center q-my-sm">
             <q-btn
               :label="$t('checkOut')"
               color="primary"
@@ -80,8 +86,8 @@
               style="width: 200px"
               @click="navigateToProduct(product.id)"
             />
-          </q-item-section>
-        </q-item>
+          </div>
+        </div>
       </q-carousel-slide>
     </q-carousel>
   </client-only>
